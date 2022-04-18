8466a2a3e887267a1df0d7162a12c2360309d1bd
Esetto <eseikahsai@gmail.com>
2022-04-17 20:45:17 -0500

    esei "product image"

--- src/main/java/net/waa/g7backend/productimage/controller/ProductImageController.java
+++ src/main/java/net/waa/g7backend/productimage/controller/ProductImageController.java
@@ -1,7 +1,42 @@
 package net.waa.g7backend.productimage.controller;
 
-import org.springframework.stereotype.Controller;
+import lombok.RequiredArgsConstructor;
+import net.waa.g7backend.productimage.dto.ProductImageDto;
 
-@Controller
+import net.waa.g7backend.productimage.service.ProductImageService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/product-images")
+@RequiredArgsConstructor
 public class ProductImageController {
+
+    private final ProductImageService imageService;
+
+    @GetMapping
+    public List<ProductImageDto> getAll(){
+        return imageService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ProductImageDto get(@PathVariable("id") long id){
+        return imageService.findById(id);
+    }
+
+    @PostMapping
+    public ProductImageDto add(@RequestBody ProductImageDto dto){
+        return imageService.add(dto);
+    }
+
+    @PutMapping("/{id}")
+    public ProductImageDto update(@PathVariable("id") long id, @RequestBody ProductImageDto dto){
+        return imageService.updateById(id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") long id){
+        imageService.deleteById(id);
+    }
 }
